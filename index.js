6951f511e8900a7ae8e2138aa535bdd68526c786
josepabellana <josep.abe.pu@gmail.com>
2023-05-26 20:35:08 +0200

    x

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { createClient } from '@clickhouse/client'
 
 const client = createClient({
-  /* configuration */
+    host: process.env.CLICKHOUSE_HOST ?? 'http://localhost:8123',
+    username: process.env.CLICKHOUSE_USER ?? 'default',
+    password: process.env.CLICKHOUSE_PASSWORD ?? '',
 })
